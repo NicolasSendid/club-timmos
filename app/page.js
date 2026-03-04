bd1674873447fe437bdf729ff46ecdc143105d17
NicolasSendid <timmosbatignolles@gmail.com>
2026-03-04 15:11:16 +0100

    Update form labels and placeholders for recommendations

--- app/page.js
+++ app/page.js
@@ -37,12 +37,15 @@ export default function Home() {
         <input first name="apporteur_prenom" placeholder="Votre prenom" required />
         <input name="apporteur_email" placeholder="Votre email" required />
         <input name="apporteur_tel" placeholder="Votre téléphone" required />
-   <div className="container">
+  
+   return (
+    <div className="container">
       <div style={{ textAlign: "center", marginBottom: "30px" }}>
         <Image src="/logo.png" alt="TimmoS" width={180} height={180} />
-        <h1>Club TimmoS</h1>
-        <p>Apporteurs et Partenaires, recevez 300€ pour vos recommandations à la conclusion de l'acte définitif de vente!</p>
+        <h1>Personne recommandée</h1>
+        <p>Veuillez saisir les informations suivantes</p>
       </div>
+  
         <input name="prospect_nom" placeholder="Nom de la personne recommandee" required />
         <input name="prospect_prenom" placeholder="Prenom de la personne recommandee" required />
         <input name="prospect_tel" placeholder="Telephone de la personne recommandee" required />      

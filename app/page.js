77d697f96ec3155037f2f62c3a8e376127803811
NicolasSendid <timmosbatignolles@gmail.com>
2026-03-04 15:09:09 +0100

    Update page.js

--- app/page.js
+++ app/page.js
@@ -32,22 +32,21 @@ export default function Home() {
         <p>Apporteurs et Partenaires, recevez 300€ pour vos recommandations à la conclusion de l'acte définitif de vente!</p>
       </div>
 
-    <div className="container">
-      <div style={{ textAlign: "center", marginBottom: "30px" }}>
-        <Image src="/logo.png" alt="TimmoS" width={180} height={180} />
-        <h1>Club TimmoS</h1>
-        <p>Apporteurs et Partenaires, recevez 300€ pour vos recommandations à la conclusion de l'acte définitif de vente!</p>
-      </div>
-
       <form onSubmit={handleSubmit}>
         <input name="apporteur_nom" placeholder="Votre nom" required />
         <input first name="apporteur_prenom" placeholder="Votre prenom" required />
         <input name="apporteur_email" placeholder="Votre email" required />
         <input name="apporteur_tel" placeholder="Votre téléphone" required />
-  
+   <div className="container">
+      <div style={{ textAlign: "center", marginBottom: "30px" }}>
+        <Image src="/logo.png" alt="TimmoS" width={180} height={180} />
+        <h1>Club TimmoS</h1>
+        <p>Apporteurs et Partenaires, recevez 300€ pour vos recommandations à la conclusion de l'acte définitif de vente!</p>
+      </div>
         <input name="prospect_nom" placeholder="Nom de la personne recommandee" required />
+        <input name="prospect_prenom" placeholder="Prenom de la personne recommandee" required />
+        <input name="prospect_tel" placeholder="Telephone de la personne recommandee" required />      
         <input name="prospect_email" placeholder="Email de la personne recommandee" required />
-        <input name="prospect_tel" placeholder="Telephone de la personne recommandee" required />
         <input name="prospect_adresse" placeholder="Adresse de la personne recommandee" required />
         <textarea
           name="contact_recommande"

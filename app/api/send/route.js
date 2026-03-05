e5838941b43c5636f9de888c5aa675fc452fe47d
NicolasSendid <timmosbatignolles@gmail.com>
2026-03-05 10:35:20 +0100

    Update route.js

--- app/api/send/route.js
+++ app/api/send/route.js
@@ -13,9 +13,7 @@ export async function POST(req) {
   const dossier = "TIM-" + Date.now();
 
  const transporter = nodemailer.createTransport({
-  host: "smtp.gmail.com",
-  port: 465,
-  secure: true,
+  service: "gmail",
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
